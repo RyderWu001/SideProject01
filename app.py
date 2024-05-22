beb9a87f58edf04ca65a931871dcfada66164340
RyderWu001 <147614277+RyderWu001@users.noreply.github.com>
2024-05-22 15:31:44 +0800

    新增html

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Ruby is beautiful"
+    return render_template('home.html')
 
-@app.route('/test')
-def test():
-    return 'This is a test'
+@app.route('/test/<name>')
+def test(name):
+    return f'This is a test for {name}'
+
 
 if __name__ == "__main__":
     app.run()
