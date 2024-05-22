635a9cac63e18fbc449892945a6587d8dce179f0
RyderWu001 <147614277+RyderWu001@users.noreply.github.com>
2024-05-22 15:35:37 +0800

    3

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -10,7 +10,14 @@ def home():
 def test(name):
     return f'This is a test for {name}'
 
+@app.route('/form')
+def form():
+    return render_template('form.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    name = request.form['name']
+    return f'Hello, {name}!'
 
 if __name__ == "__main__":
-    app.run()
-  
+    app.run(debug=True)
